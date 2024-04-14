40ef393070700f9013278a0aa1de96b6e85b6bdd
H1kosso <marcin.hikosso@gmail.com>
2024-04-14 16:28:05 +0200

    Backend: basic authentication operations

---
+++ Backend/lib/models/GameSessionModel.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface IGameSession extends Document {
+    sessionId: Number;
+    gameID: string;
+    location: string;
+    playersId: string; //coma separated list
+    winnerId: string;
+}
+
+const GameSessionSchema: Schema = new Schema({
+    sessionId:{ type: Number, required: true },
+    gameID: { type: Number, required: true },
+    location: { type: String, required: true },
+    playersId: { type: String, required: true },
+    winnerId: { type: Number, required: true }
+});
+
+const GameSessionModel = mongoose.model<IGameSession>('onBoard', GameSessionSchema);
+
+export default GameSessionModel;
