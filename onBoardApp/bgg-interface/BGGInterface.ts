f2f3c553817bad67b450b1a4edadf0bbb4eaaea1
H1kosso <marcin.hikosso@gmail.com>
2024-05-05 10:15:20 +0200

    Fixed undefines in bgg response

--- onBoardApp/bgg-interface/BGGInterface.ts
+++ onBoardApp/bgg-interface/BGGInterface.ts
@@ -33,14 +33,15 @@ async function searchBoardGame(query) {
 
         thingJsonData.items.item.forEach(game => {
             let gameData = {
-                categories: game.link.filter(link => link.$.type === 'boardgamecategory').map(link => link.$.value),
-                description: game.description[0],
-                image: game.image[0],
-                name: game.name[0].$.value,
-                id: game.$.id
+                categories: game.link ? game.link.filter(link => link.$.type === 'boardgamecategory').map(link => link.$.value) : [],
+                description: game.description ? game.description[0] : "",
+                image: game.image ? game.image[0] : "",
+                name: game.name ? game.name[0].$.value : "",
+                id: game.$.id ? game.$.id : ""
             };
             games.push(gameData);
         });
+        console.log(games)
         return games;
     } catch (error) {
         console.error('Error fetching data:', error);
@@ -57,22 +58,22 @@ async function getBoardGameById(id: string) {
 
         const item = jsonData.items.item[0];
         const gameData = {
-            name: item.name.find((name: { $: { type: string } }) => name.$.type === 'primary').$.value,
-            thumbnail: item.thumbnail[0],
-            image: item.image[0],
-            description: item.description[0],
-            yearPublished: item.yearpublished[0].$.value,
-            minPlayers: item.minplayers[0].$.value,
-            maxPlayers: item.maxplayers[0].$.value,
-            playingTime: item.playingtime[0].$.value,
-            minPlayTime: item.minplaytime[0].$.value,
-            maxPlayTime: item.maxplaytime[0].$.value,
-            minAge: item.minage[0].$.value,
-            categories: item.link.filter((link: { $: { type: string } }) => link.$.type === 'boardgamecategory').map((link: { $: { value: string } }) => link.$.value),
-            mechanics: item.link.filter((link: { $: { type: string } }) => link.$.type === 'boardgamemechanic').map((link: { $: { value: string } }) => link.$.value),
-            designers: item.link.filter((link: { $: { type: string } }) => link.$.type === 'boardgamedesigner').map((link: { $: { value: string } }) => link.$.value),
-            artists: item.link.filter((link: { $: { type: string } }) => link.$.type === 'boardgameartist').map((link: { $: { value: string } }) => link.$.value),
-            publishers: item.link.filter((link: { $: { type: string } }) => link.$.type === 'boardgamepublisher').map((link: { $: { value: string } }) => link.$.value)
+            name: item.name && item.name.find((name: { $: { type: string } }) => name.$.type === 'primary') ? item.name.find((name: { $: { type: string } }) => name.$.type === 'primary').$.value : "",
+            thumbnail: item.thumbnail ? item.thumbnail[0] : "",
+            image: item.image ? item.image[0] : "",
+            description: item.description ? item.description[0] : "",
+            yearPublished: item.yearpublished && item.yearpublished[0] ? item.yearpublished[0].$.value : "",
+            minPlayers: item.minplayers && item.minplayers[0] ? item.minplayers[0].$.value : "",
+            maxPlayers: item.maxplayers && item.maxplayers[0] ? item.maxplayers[0].$.value : "",
+            playingTime: item.playingtime && item.playingtime[0] ? item.playingtime[0].$.value : "",
+            minPlayTime: item.minplaytime && item.minplaytime[0] ? item.minplaytime[0].$.value : "",
+            maxPlayTime: item.maxplaytime && item.maxplaytime[0] ? item.maxplaytime[0].$.value : "",
+            minAge: item.minage && item.minage[0] ? item.minage[0].$.value : "",
+            categories: item.link && item.link.filter((link: { $: { type: string } }) => link.$.type === 'boardgamecategory').map((link: { $: { value: string } }) => link.$.value) || [],
+            mechanics: item.link && item.link.filter((link: { $: { type: string } }) => link.$.type === 'boardgamemechanic').map((link: { $: { value: string } }) => link.$.value) || [],
+            designers: item.link && item.link.filter((link: { $: { type: string } }) => link.$.type === 'boardgamedesigner').map((link: { $: { value: string } }) => link.$.value) || [],
+            artists: item.link && item.link.filter((link: { $: { type: string } }) => link.$.type === 'boardgameartist').map((link: { $: { value: string } }) => link.$.value) || [],
+            publishers: item.link && item.link.filter((link: { $: { type: string } }) => link.$.type === 'boardgamepublisher').map((link: { $: { value: string } }) => link.$.value) || []
         };
 
         console.log( JSON.stringify(gameData, null, 2));
