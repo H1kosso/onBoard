05f80fe895a1d48bb8fd74480daf26c2244b2827
H1kosso <marcin.hikosso@gmail.com>
2024-05-08 20:02:49 +0200

    Collection screen

--- onBoardApp/screens/Collection.tsx
+++ onBoardApp/screens/Collection.tsx
@@ -1,13 +1,98 @@
-import {useTheme} from "react-native-paper";
-import {SafeAreaView, Text} from "react-native";
-import {searchBoardGame} from "../bgg-interface/BGGInterface";
+import {Avatar, Card, Paragraph, useTheme} from "react-native-paper";
+import { Button, FlatList, SafeAreaView, Text, View } from "react-native";
+import { getBoardGameById, searchBoardGame } from "../bgg-interface/BGGInterface";
+import { useEffect, useState } from "react";
+import env from "../env";
+import axios from 'axios';
 
 export function Collection() {
-    const paperTheme = useTheme();
+    const localhost = env.IP_ADDRESS;
+    const [collection, setCollection] = useState([]);
+    const [games, setGames] = useState([]);
+    const [finished, setFinished] = useState(false);
+
+    useEffect(() => {
+        const url = `${localhost}/api/collection?username=test`;
+        axios.get(url)
+            .then(response => {
+                setCollection(response.data);
+
+                const gameIds = response.data.map(game => game.gameId);
+                const gamePromises = gameIds.map(id => getBoardGameById(id));
+
+                Promise.all(gamePromises)
+                    .then(gameDetails => {
+                        // Flatten the array of arrays into a single array
+                        const flattenedGameDetails = gameDetails.flat();
+                        setGames(mergeGameData(flattenedGameDetails, response.data));
+                        setFinished(true);
+                    })
+                    .catch(error => {
+                        console.error('Error fetching game details:', error);
+                    });
+            })
+            .catch(error => {
+                console.error("Błąd:", error);
+            });
+    }, []);
+
+    function mergeGameData(gameLog, ownershipLog) {
+        const gameOwnershipDict = {};
+        ownershipLog.forEach(entry => {
+            gameOwnershipDict[entry.gameId.toString()] = entry;
+        });
+
+        const mergedData = gameLog.map(game => {
+            const ownershipEntry = gameOwnershipDict[game.gameId];
+            if (ownershipEntry) {
+                return { ...game, ...ownershipEntry };
+            } else {
+                return game;
+            }
+        });
+        return mergedData;
+    }
 
     return (
-        <SafeAreaView style={{ backgroundColor: paperTheme.colors.background }}>
-           <Text>Collection</Text>
+        <SafeAreaView>
+            {
+                games !== undefined
+                    ? <FlatList
+                        data={games}
+                        renderItem={({ item }) => <GameItem itemProps={item}/>} />
+                    : null
+            }
+
         </SafeAreaView>
+    );
+}
+
+function GameItem(props: GameItemProps) {
+    const iconSize = 54;
+    const itemProps = props.itemProps;
+    const helperUri = "https://cf.geekdo-images.com/NaVK216SnjDz3VLr5kKOAg__original/img/_fR_-LU6T4zfa901SvBAx3V8O3k=/0x0/filters:format(jpeg)/pic350302.jpg";
+    return (
+        <Card
+            style={{ marginVertical: 4, marginHorizontal: 8 }}>
+
+            <Card.Title
+                title={itemProps.title}
+                subtitle={itemProps.categories.join(', ')}
+                left={(props) =>
+                    <Avatar.Image
+                        {...props}
+                        size={iconSize}
+                        source={{ uri: itemProps.imageUrl ? itemProps.imageUrl : helperUri }}
+                    />
+                }
+                leftStyle={{ marginRight: 32 }}
+            />
+            <Card.Content>
+                <Paragraph>Buy Price: {itemProps.buyPrice}</Paragraph>
+                <Paragraph>When Bought: {itemProps.whenBought}</Paragraph>
+                <Paragraph>Owned: {itemProps.owned ? 'Yes' : 'No'}</Paragraph>
+
+            </Card.Content>
+        </Card>
     );
 }
